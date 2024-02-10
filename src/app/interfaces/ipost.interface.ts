ec63e429b3dcbf405c8a212d3caba922d54d7a44
jgamonal <josue.gamonal@gmail.com>
2024-02-10 19:55:57 +0100

    funcionando todo y con media queries

---
+++ src/app/interfaces/ipost.interface.ts
@@ -0,0 +1,6 @@
+export interface Ipost {
+	titulo: string;
+	imagen: string;
+	texto: string;
+	fecha: string;
+}
--- src/app/modules/blog/blog.component.ts
+++ src/app/modules/blog/blog.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Ipost } from '../../interfaces/ipost.interface';
 
 @Component({
   selector: 'app-blog',
@@ -9,23 +10,27 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
-	arrPost: any[] =[
-		{'titulo':"",
-		'imagen': "",
-		'texto': "",
-		'fecha': ""}
+	arrPost: Ipost[] =[
+		{titulo:"Noticia 1",
+		imagen: "https://picsum.photos/300/300",
+		texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum erat sed orci efficitur, nec porttitor eros fermentum. Fusce hendrerit ipsum orci, ac aliquet nisl commodo sed.",
+		fecha: "05/02/2024"},
+		{titulo:"Noticia 2",
+		imagen: "https://picsum.photos/300/300",
+		texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum erat sed orci efficitur, nec porttitor eros fermentum. Fusce hendrerit ipsum orci, ac aliquet nisl commodo sed.",
+		fecha: "06/02/2024"}
 	]
 
-	newPost: any = {
-		'titulo':"",
-		'imagen': "",
-		'texto': "",
-		'fecha': ""
+	newPost: Ipost = {
+		titulo:"",
+		imagen: "",
+		texto: "",
+		fecha: ""
 	}
 
 	guardar(){
 		if(this.newPost.titulo == "" || this.newPost.imagen == "" || this.newPost.texto == "" || this.newPost.fecha == ""){
-		alert("Faltan datos");
+		alert("Por favor, rellena todos los campos");
 		return;
 		}
 
@@ -33,30 +38,30 @@ export class BlogComponent {
 
 		// hay que romper la referencia para que no repita los valores inicializando el objeto vacío
 		this.newPost = {
-			'titulo':"",
-			'imagen': "",
-			'texto': "",
-			'fecha': ""
+			titulo:"",
+			imagen: "",
+			texto: "",
+			fecha: ""
 		}
 
 	}
 
 	cargarDatos(): string {
 		let html = "";
-		this.arrPost.forEach((post) => {
+		this.arrPost.forEach((post: any) => {
 			html += `
-			<div class="wrapper">
-			<div class="blog_item">
+
+			<div class="blog_post">
 				<div class="blog_item_img">
-					<img src="${post.imagen}" alt="">
+					<img class="blog_item_img" src="${post.imagen}" alt="">
 				</div>
 				<div class="blog_item_content">
-					<h2>${post.titulo}</h2>
-					<p>${post.texto}</p>
-					<p>${post.fecha}</p>
+				<p class="blog_item_fecha">Fecha de publicación ${post.fecha}</p>
+				<h2 class="blog_item_titulo">${post.titulo}</h2>
+				<p class="blog_item_texto">${post.texto}</p>
 				</div>
 				</div>
-				</div>`
+				`
 		})
 		return html;
 	}
