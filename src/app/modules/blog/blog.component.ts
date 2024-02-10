0dfabc870dfd91a57ea16c68cb01595173c43474
jgamonal <josue.gamonal@gmail.com>
2024-02-10 12:12:30 +0100

    control de errores de input funcionando

--- src/app/modules/blog/blog.component.ts
+++ src/app/modules/blog/blog.component.ts
@@ -9,7 +9,12 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
-	arrPost: any[] =[{}]
+	arrPost: any[] =[
+		{'titulo':"",
+		'imagen': "",
+		'texto': "",
+		'fecha': ""}
+	]
 
 	newPost: any = {
 		'titulo':"",
@@ -19,15 +24,40 @@ export class BlogComponent {
 	}
 
 	guardar(){
+		if(this.newPost.titulo == "" || this.newPost.imagen == "" || this.newPost.texto == "" || this.newPost.fecha == ""){
+		alert("Faltan datos");
+		return;
+		}
+
 		this.arrPost.push(this.newPost);
+
+		// hay que romper la referencia para que no repita los valores inicializando el objeto vacío
 		this.newPost = {
 			'titulo':"",
 			'imagen': "",
 			'texto': "",
 			'fecha': ""
 		}
-		console.log(this.arrPost);
 
 	}
 
+	cargarDatos(): string {
+		let html = "";
+		this.arrPost.forEach((post) => {
+			html += `
+			<div class="wrapper">
+			<div class="blog_item">
+				<div class="blog_item_img">
+					<img src="${post.imagen}" alt="">
+				</div>
+				<div class="blog_item_content">
+					<h2>${post.titulo}</h2>
+					<p>${post.texto}</p>
+					<p>${post.fecha}</p>
+				</div>
+				</div>
+				</div>`
+		})
+		return html;
+	}
 }
