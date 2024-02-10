e0a11af03d5c68aeef31b4584bb40ab9e5e0afd8
jgamonal <josue.gamonal@gmail.com>
2024-02-10 11:19:31 +0100

    formulario recoge datos

--- src/app/modules/blog/blog.component.ts
+++ src/app/modules/blog/blog.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
+	arrPost: any[] =[{}]
+
+	newPost: any = {
+		'titulo':"",
+		'imagen': "",
+		'texto': "",
+		'fecha': ""
+	}
+
+	guardar(){
+		this.arrPost.push(this.newPost);
+		this.newPost = {
+			'titulo':"",
+			'imagen': "",
+			'texto': "",
+			'fecha': ""
+		}
+		console.log(this.arrPost);
+
+	}
 
 }
